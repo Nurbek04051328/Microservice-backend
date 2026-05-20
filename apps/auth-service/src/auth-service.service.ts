2ae2517d0a1ec442a55a3057be5c9ce3838e1839
Nurbek04051328 <bek050613@mail.ru>
2026-05-20 17:38:10 +0500

    feat: added kafka and docker

--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthServiceService {
+export class AuthServiceService implements OnModuleInit {
+  constructor(
+    @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    // connect to kafka when module initilaizes
+      await this.kafkaClient.connect();
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async simulateUserRegistration(email: string) {
+    // publish event to kafka
+    this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
+      email,
+      timestamp: new Date().toISOString(),
+    });
+
+    return { message: `User registred:  ${email}` };
   }
 }
---
+++ libs/kafka/src/index.ts
@@ -0,0 +1,6 @@
+export * from './kafka.module';
+
+
+// Constants
+export * from './constants/kafka.constants';
+
