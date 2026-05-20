00d32b484d02c53876ac26877f29ef32ed4c18c8
Nurbek04051328 <bek050613@mail.ru>
2026-05-20 16:04:42 +0500

    feat: libs/comman/src constants, dto, interfaces, utils added

--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -1,8 +1,9 @@
+import { SERVCIES_PORTS } from '@app/common';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `API Gateway is running on port ${SERVCIES_PORTS.API_GATEWAY}`;
   }
 }
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,12 @@
 export * from './common.module';
 export * from './common.service';
+
+
+// Constants
+export * from './constants';
+
+// Interfaces
+export * from './interfaces';
+
+// Utils
+export * from './utils';
